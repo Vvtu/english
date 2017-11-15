6709c424cdf827d98b63909b2a2a8cb0257fa558
Vvtu <vvtu@icloud.com>
2017-11-15 16:36:45 +0300

    add    handleBackClicked  and  handleForwardClicked

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import ThemeProvider from 'arui-feather/theme-provider';
 import Label from 'arui-feather/label';
 import Button from 'arui-feather/button';
 import CheckBox from 'arui-feather/checkbox';
+import Icon from 'arui-feather/icon';
+
+import IconBack from './iconback.svg';
 
 import { dictionary, settings } from './data/dictionary.json';
 
@@ -27,6 +30,28 @@ class App extends PureComponent {
     console.error(error, info);
   }
 
+  handleBackClicked = () => {
+    console.log('handleBackClicked')
+    let newActiveIndex = this.state.activeIndex - 1;
+    if (newActiveIndex < 0) {
+      newActiveIndex = dictionary.length - 1;
+    }
+    this.setState(
+      {activeIndex: newActiveIndex}
+    );
+  };
+  handleForwardClicked = () => {
+    console.log('handleForwardClicked')
+
+    let newActiveIndex = this.state.activeIndex + 1;
+    if (newActiveIndex >= dictionary.length) {
+      newActiveIndex = 0;
+    }
+    this.setState(
+      {activeIndex: newActiveIndex}
+    );
+  };
+
   render() {
     const { activeIndex, showEnglish } = this.state;
     const { theme, size } = settings || {};
@@ -35,15 +60,33 @@ class App extends PureComponent {
     const english = showEnglish && Object.values(activeObj || {})[0];
 
     return (
-      <div className={'theme__backgroundcolor_' + theme }>
+      <div className={'theme__backgroundcolor_' + theme}>
         <ThemeProvider theme={theme}>
           <div className="app__grid">
             <div className="app__info">
               <div className="app__info2">
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
-                <div>4</div>
+                <img
+                  alt={'back'}
+                  height={32}
+                  src={IconBack}
+                  width={32}
+                  onClick={this.handleBackClicked}
+                />
+
+                <div>
+                  <Icon name="action-fail" colored={true} size={size} />
+                </div>
+                <div>
+                  <Icon name="action-ok" colored={true} size={size} />
+                </div>
+                <img
+                  alt={'back'}
+                  height={32}
+                  src={IconBack}
+                  className="invert__horizontal"
+                  width={32}
+                  onClick={this.handleForwardClicked}
+                />
               </div>
             </div>
             <div className="app__russian">
