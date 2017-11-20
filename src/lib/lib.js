eafac8639b2481295b1bd22209b5604f28efe06b
Vvtu <vvtu@icloud.com>
2017-11-20 13:10:23 +0300

    fix arrayRandomOrder

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -20,7 +20,7 @@ export const arrayRandomOrder = (arr000) => {
   const arr = arr000.slice(0);
   const len = arr.length;
   for (let i = 0; i < len - 1; i += 1) {
-    const j = getRandomInt(i + 1, len);
+    const j = getRandomInt(i , len);
     const swap = arr[i];
     arr[i] = arr[j];
     arr[j] = swap;
