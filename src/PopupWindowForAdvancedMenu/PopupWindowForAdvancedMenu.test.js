f9ebf768897485957278a175a94844b4947abce7
Vyacheslav Tuev <vyacheslavtuev@192.168.22.105>
2017-12-17 21:26:22 +0300

    add test to popup menu

---
+++ src/PopupWindowForAdvancedMenu/PopupWindowForAdvancedMenu.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import PopupWindowForAdvancedMenu from './PopupWindowForAdvancedMenu';
+
+// const dictionary = [ { 'Русский текст': 'Russian text' } ];
+
+describe('PopupWindowForAdvancedMenu test   ', () => {
+    it('test 1', () => {
+        const component = renderer.create(<PopupWindowForAdvancedMenu  />);
+        const result = component.toJSON();
+        expect(result).toMatchSnapshot();
+    });
+});
