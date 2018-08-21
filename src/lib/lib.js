5b5fa31e09a9c06fed144da29ecef9f6e8dde159
Viacheslav Tuev <vvtu@icloud.com>
2018-08-21 16:58:07 +0300

    fix reshuffle

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,36 +1,27 @@
 export const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  //  The  min is inclusive  and the max is exclusive
-  return Math.floor(Math.random() * (max - min)) + min;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	//  The  min is inclusive  and the max is exclusive
+	return Math.floor(Math.random() * (max - min)) + min;
 };
 
-export const arrayRandomOrder = (arr000, maxLength) => {
-  const arr = arr000.slice(0);
-  const len = arr.length;
-
-  const numberOfElementsToSort = Math.min(len - 1, maxLength ? maxLength : 10000000000);
-
-  for (let i = 0; i < numberOfElementsToSort; i += 1) {
-    const j = getRandomInt(i, len);
-    const swap = arr[i];
-    arr[i] = arr[j];
-    arr[j] = swap;
-  }
-  return arr;
-};
+const reshuffle = (dic) =>
+	dic
+		.map((item) => [ Math.random(), item ])
+		.sort((a, b) => a[0] - b[0])
+		.map((elem) => elem[1]);
 
 export const filterDeletedOff = (arr) => {
-  const result = (arr || []).filter((elem) => {
-    const russian = Object.keys(elem || {})[0];
-    if (russian) {
-      const item = localStorage.getItem(russian);
-      return item !== 'd';
-    } else {
-      return false;
-    }
-  });
-  return result;
+	const result = (arr || []).filter((elem) => {
+		const russian = Object.keys(elem || {})[0];
+		if (russian) {
+			const item = localStorage.getItem(russian);
+			return item !== 'd';
+		} else {
+			return false;
+		}
+	});
+	return result;
 };
 
 const DICT = 'DICT';
@@ -39,26 +30,26 @@ const DICT_NUMBER = '2';
 const OLD_LENGTH = 3;
 
 export const getDictionaryWithMix = (dictionaries) => {
-  const whichDict = localStorage.getItem(DICT);
-  let d;
-  let oldD;
-  if (whichDict === DICT_NUMBER) {
-    d = dictionaries.dictionary2;
-    oldD = dictionaries.oldDictionary2;
-  } else {
-    d = dictionaries.dictionary1;
-    oldD = dictionaries.oldDictionary1;
-  }
-  d = arrayRandomOrder(filterDeletedOff(d, null));
-  oldD = arrayRandomOrder(filterDeletedOff(oldD, OLD_LENGTH)).slice(0, OLD_LENGTH);
+	const whichDict = localStorage.getItem(DICT);
+	let d;
+	let oldD;
+	if (whichDict === DICT_NUMBER) {
+		d = dictionaries.dictionary2;
+		oldD = dictionaries.oldDictionary2;
+	} else {
+		d = dictionaries.dictionary1;
+		oldD = dictionaries.oldDictionary1;
+	}
+	d = reshuffle(filterDeletedOff(d, null));
+	oldD = reshuffle(filterDeletedOff(oldD, OLD_LENGTH)).slice(0, OLD_LENGTH);
 
-  return oldD.concat(d);
+	return oldD.concat(d);
 };
 
 export const handleDictClicked = (number) => {
-  if (number === 2) {
-    localStorage.setItem(DICT, DICT_NUMBER);
-  } else {
-    localStorage.setItem(DICT, '1');
-  }
+	if (number === 2) {
+		localStorage.setItem(DICT, DICT_NUMBER);
+	} else {
+		localStorage.setItem(DICT, '1');
+	}
 };
